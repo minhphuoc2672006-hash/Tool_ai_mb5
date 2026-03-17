248f060a71ce1c93ddcfacfb47da0967319eefbe
minhphuoc2672006-hash <minhphuoc2672006@gmail.com>
2026-03-17 16:13:20 +0700

    Create ocr.py

---
+++ ocr.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+
+def read_image(path):
+    try:
+        text = pytesseract.image_to_string(Image.open(path))
+
+        numbers = []
+        for t in text.split():
+            if t.isdigit():
+                numbers.append(int(t))
+
+        return numbers[-20:]
+
+    except:
+        return []
