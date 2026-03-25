0fccec1fd73ec56ee21237b1bc477fb515b81e9f
minhphuoc2672006-hash <minhphuoc2672006@gmail.com>
2026-03-25 19:21:53 +0700

    Delete money.py

--- money.py
+++
@@ -1,8 +0,0 @@
-def split_money(total):
-    return {
-        "Lệnh 1 (chính)": int(total * 0.3),
-        "Lệnh 2": int(total * 0.2),
-        "Lệnh 3": int(total * 0.2),
-        "Lệnh 4": int(total * 0.1),
-        "Dự phòng": int(total * 0.2),
-    }
